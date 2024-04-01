60744c62b9a781d0d84f3984c89c772eb54fdbeb
Jack Boylan <boylan.jack@gmail.com>
2024-04-01 21:40:13 +0200

    Correctly accumulates NER indices in DocRED after combining sentences

--- data/process_docred.py
+++ data/process_docred.py
@@ -1,6 +1,8 @@
 from datasets import load_dataset
 import json
 
+NUM_EXAMPLES = 10
+
 '''
 
 # DocRED paper: https://arxiv.org/pdf/1906.06127.pdf
@@ -30,42 +32,80 @@ def map_entities(doc):
 
 
 def expand_labels(doc, entity_mapping):
-    # Expand the labels to individual mentions
     expanded_labels = []
     labels = doc['labels']
-    for i, relation in enumerate(zip(labels['head'], labels['tail'], labels['relation_id'], labels['relation_text'], labels['evidence'])):
-        head_id, tail_id, relation_id, relation_text, evidence = relation
-        # Expand the head and tail mentions
+    
+    # Function to get cumulative position of an entity mention
+    def get_cumulative_position(mention, cumulative_sentence_lengths):
+        return cumulative_sentence_lengths[mention['sent_id']] + mention['pos'][0], \
+               cumulative_sentence_lengths[mention['sent_id']] + mention['pos'][1]
+    
+    # Calculate cumulative sentence lengths for indexing
+    cumulative_sentence_lengths = [0]
+    for sentence in doc['sents']:
+        cumulative_sentence_lengths.append(cumulative_sentence_lengths[-1] + len(sentence))
+    
+    # Iterate over the relations in the labels
+    for i, (head_id, tail_id, relation_id, relation_text, evidence) in enumerate(zip(
+        labels['head'], labels['tail'], labels['relation_id'], labels['relation_text'], labels['evidence'])):
+        
+        # Iterate over all mentions for the head entity
         for head_mention in doc['vertexSet'][head_id]:
+            head_start_index, head_end_index = get_cumulative_position(head_mention, cumulative_sentence_lengths)
+            
+            # Iterate over all mentions for the tail entity
             for tail_mention in doc['vertexSet'][tail_id]:
+                tail_start_index, tail_end_index = get_cumulative_position(tail_mention, cumulative_sentence_lengths)
+                
+                # Construct the relation for each mention
                 expanded_relation = {
-                    'head': head_mention,
-                    'tail': tail_mention,
+                    'head': {
+                        'name': head_mention['name'],
+                        'position': [head_start_index, head_end_index],
+                        'type': head_mention['type']
+                    },
+                    'tail': {
+                        'name': tail_mention['name'],
+                        'position': [tail_start_index, tail_end_index],
+                        'type': tail_mention['type']
+                    },
                     'relation_id': relation_id,
                     'relation_text': relation_text,
                     'evidence': evidence
                 }
                 expanded_labels.append(expanded_relation)
 
+
+    # sort for viewing (not necessary) 
+    expanded_labels = sorted(expanded_labels, key=lambda x: (x['head']['name'], x['head']['position'][0], x['tail']['position'][0]))
     return expanded_labels
 
-def get_ner_from_entity_mapping(entity_mapping):
+
+def get_ner_from_entity_mapping(entity_mapping, doc):
     ner = []
+    cumulative_offset = 0  
     for entity_id, mention in entity_mapping.items():
-        ner.append([mention['pos'][0], mention['pos'][1], mention['type']])
+        # Find the cumulative offset up to the sentence the entity is in
+        cumulative_offset = sum(len(sent) for sent in doc['sents'][:mention['sent_id']])
+        # Adjust the entity's position with the cumulative offset
+        ner.append([cumulative_offset + mention['pos'][0], cumulative_offset + mention['pos'][1], mention['type'], mention['name']])
+    ner = sorted(ner, key=lambda x: x[0])
     return ner
+
 
 #
 dataset = load_dataset("docred")    # features: ['relation', 'tokens', 'head', 'tail', 'names'],
 
+SPLIT = 'validation'
+
 data = []
-for i in range(10):
-    doc = dataset['validation'][i]
+for i in range(NUM_EXAMPLES):
+    doc = dataset[SPLIT][i]
     example_row = {}
 
     entity_mapping = map_entities(doc)
 
-    example_row['ner'] = get_ner_from_entity_mapping(entity_mapping)  #  "ner": [[3,6,"LOC"], [7,9,"PER"]]
+    example_row['ner'] = get_ner_from_entity_mapping(entity_mapping, doc)  #  "ner": [[3,6,"LOC"], [7,9,"PER"]]
     example_row['relations'] = expand_labels(doc, entity_mapping)
 
     # Combine the list of sentences into one list of tokens
@@ -73,9 +113,10 @@ for i in range(10):
 
     data.append(example_row)
 
-# # Output the expanded labels in a JSON format for the user to verify
-# expanded_labels_json = print(json.dumps(data[1], indent=2))
-
+# for ent in data[0]['ner']: print(f"{ent} => {data[0]['tokenized_text'][ent[0]:ent[1]]}")
+    
+# for rel in data[0]['relations']: print(f"{rel['head']['name']} {rel['head']['position']} -> {rel['relation_text']} -> {rel['tail']['name']} {rel['tail']['position']}")
+    
 # save to a jsonl file
 with open('docred_expanded.jsonl', 'w') as f:
     for item in data:
