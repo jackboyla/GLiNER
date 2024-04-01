b5327985498b6ff1047c24177f09e57f2140e6a3
Jack Boylan <boylan.jack@gmail.com>
2024-04-01 21:43:02 +0200

    Implements relation batch data collation

--- gliner/modules/base.py
+++ gliner/modules/base.py
@@ -8,6 +8,23 @@ from torch.utils.data import DataLoader
 import random
 import os
 
+def generate_entity_pairs_indices(self, span_idx):
+    num_entities = span_idx.size(0)  # [num_ents, 2]
+
+    # Expand and tile to create all possible pairs
+    span_idx_expanded = span_idx.unsqueeze(1).expand(-1, num_entities, -1)  #  ([num_entities, num_entities, 2])
+    span_idx_tiled = span_idx.unsqueeze(0).expand(num_entities, -1, -1)     #  ([num_entities, num_entities, 2])
+
+    # pair_reps = torch.cat([span_idx_expanded, span_idx_tiled], dim=2)
+
+    # Now, we need to filter out the self-pairs and duplicates. We use an upper triangular mask for this
+    triu_mask = torch.triu(torch.ones(num_entities, num_entities), diagonal=1).bool()  #  ([ num_entities, num_entities ])
+
+    # num_unique_pairs is the total number of unique pairs that can be formed from num_entities, excluding self-pairs
+    combined_pairs = torch.stack((span_idx_expanded[triu_mask], span_idx_tiled[triu_mask]), dim=1)  
+
+    return combined_pairs  #  ([num_unique_pairs, 2 ->start_index, 2 ->end_index])
+
 
 class InstructBase(nn.Module):
     def __init__(self, config):
@@ -21,56 +38,96 @@ class InstructBase(nn.Module):
             if span[2] in classes_to_id:
                 dict_tag[(span[0], span[1])] = classes_to_id[span[2]]
         return dict_tag
+    
+    def get_rel_labels(self, relations_idx, relations, rel_label_dict):
+        # get the class for each relation pair
+        labelled_rel_dict = {
+            (tuple(rel['head']['position']), tuple(rel['tail']['position'])) : rel['relation_text'] for rel in relations
+        }
+        relations_idx = relations_idx.tolist()
 
-    def preprocess_spans(self, tokens, ner, classes_to_id):
+        # assign label if in dataset else -1
+        rel_labels = []
+        for rel in relations_idx:
+            head_idx, tail_idx = tuple(rel[0]), tuple(rel[1])
+            if (head_idx, tail_idx) in labelled_rel_dict:
+                label = rel_label_dict[labelled_rel_dict[(head_idx, tail_idx)]]
+                rel_labels.append(label)
+            else:
+                rel_labels.append(0)
+        
+        return rel_labels
+    
+
+    def preprocess_spans(self, tokens, ner, classes_to_id, relations):
 
         max_len = self.base_config.max_len
 
         if len(tokens) > max_len:
-            length = max_len
+            seq_length = max_len
             tokens = tokens[:max_len]
         else:
-            length = len(tokens)
+            seq_length = len(tokens)
 
         spans_idx = []
+
         if os.environ.get('TASK') == 'ner':
-            for i in range(length):
+            for i in range(seq_length):
                 spans_idx.extend([(i, i + j) for j in range(self.max_width)])
+            dict_lab = self.get_dict(ner, classes_to_id) if ner else defaultdict(int)  # {(0, 5): 1, (0, 0): 0, (0, 1): 0, ...}
+
+            # 0 for null labels
+            span_label = torch.LongTensor([dict_lab[i] for i in spans_idx])  # [num_possible_spans]
+            spans_idx = torch.LongTensor(spans_idx)                          # [num_possible_spans, 2]
+            
+            # mask for valid spans
+            valid_span_mask = spans_idx[:, 1] > seq_length - 1
+
+            # mask invalid positions
+            span_label = span_label.masked_fill(valid_span_mask, -1)  # [num_possible_spans]
+
+
         elif os.environ.get('TASK') == 'rel':
             for ner_span in ner:
                 start, end = ner_span[0], ner_span[1]
                 spans_idx.append((start, end))
+            
+            spans_idx = torch.LongTensor(spans_idx)                        # [num_possible_spans, 2]
+            relations_idx = generate_entity_pairs_indices(spans_idx)  # [num_ent_pairs, 2, 2]
 
-                # rel_labels need to be [B, num_entity_pairs] -- rewrite above ^
-                # get all span_idx from relations and ner
-                # assign classes to spans as dict
+            # TODO: make sure model generates relations in the same way as generate_entity_pairs_indices()
 
-        dict_lab = self.get_dict(ner, classes_to_id) if ner else defaultdict(int)  # {(0, 5): 1, (0, 0): 0, (0, 1): 0, ...}
+            unique_rel_labels = set([rel['relation_text'] for rel in relations])
+            rel_label_dict = {r: (i+1) for i, r in enumerate(unique_rel_labels)}
+            # 0 for null labels
+            rel_label = torch.LongTensor(self.get_rel_labels(relations_idx, relations, rel_label_dict))  # [num_ent_pairs]
 
-        # 0 for null labels
-        span_label = torch.LongTensor([dict_lab[i] for i in spans_idx])
-        spans_idx = torch.LongTensor(spans_idx)
+            # mask for valid spans
+            valid_span_mask = spans_idx[:, 1] > seq_length - 1
 
-        # mask for valid spans
-        valid_span_mask = spans_idx[:, 1] > length - 1
+            # mask invalid positions
+            span_label = torch.ones(spans_idx.size(0), dtype=torch.long)
+            span_label = span_label.masked_fill(valid_span_mask, -1)  # [num_possible_spans]
 
-        # mask invalid positions
-        span_label = span_label.masked_fill(valid_span_mask, -1)
 
-        import ipdb;ipdb.set_trace()
-
-        return {
+        out = {
             'tokens': tokens,
             'span_idx': spans_idx,
             'span_label': span_label,
-            'seq_length': length,
+            'seq_length': seq_length,
             'entities': ner,
+            'rel_label': rel_label if 'rel_label' in locals() else None,
         }
+        return out
 
     def collate_fn(self, batch_list, entity_types=None):
         # batch_list: list of dict containing tokens, ner
         if entity_types is None:
-            negs = self.get_negatives(batch_list, 100)
+            if os.environ['TASK'] == 'ner':
+                negs = self.get_negatives(batch_list, 100)
+            elif os.environ['TASK'] == 'rel':
+                # TODO: add negatives for relations
+                negs = self.get_negatives_rel(batch_list, 100)
             class_to_ids = []
             id_to_classes = []
             for b in batch_list:
@@ -90,10 +147,17 @@ class InstructBase(nn.Module):
                     # no negatives
                     negs_i = []
                 else:
-                    negs_i = negs[:len(b['ner']) * neg_type_ratio]
+                    if os.environ['TASK'] == 'ner':
+                        negs_i = negs[:len(b['ner']) * neg_type_ratio]
+                    elif os.environ['TASK'] == 'rel':
+                        negs_i = negs[:len(b['relations']) * neg_type_ratio]
+                    
+                if os.environ['TASK'] == 'ner':
+                    # this is the list of all possible entity types (positive and negative)
+                    types = list(set([el[2] for el in b['ner']] + negs_i))
+                elif os.environ['TASK'] == 'rel':
+                    types = list(set([el['relation_text'] for el in b['relations']] + negs_i))
 
-                # this is the list of all possible entity types (positive and negative)
-                types = list(set([el[-1] for el in b['ner']] + negs_i))
 
                 # shuffle (every epoch)
                 random.shuffle(types)
@@ -118,14 +182,14 @@ class InstructBase(nn.Module):
                 id_to_classes.append(id_to_class)
 
             batch = [
-                self.preprocess_spans(b["tokenized_text"], b["ner"], class_to_ids[i]) for i, b in enumerate(batch_list)
+                self.preprocess_spans(b["tokenized_text"], b["ner"], class_to_ids[i], b.get('relations')) for i, b in enumerate(batch_list)
             ]
 
         else:
             class_to_ids = {k: v for v, k in enumerate(entity_types, start=1)}
             id_to_classes = {k: v for v, k in class_to_ids.items()}
             batch = [
-                self.preprocess_spans(b["tokenized_text"], b["ner"], class_to_ids) for b in batch_list
+                self.preprocess_spans(b["tokenized_text"], b["ner"], class_to_ids, b.get('relations')) for b in batch_list
             ]
 
         span_idx = pad_sequence(
@@ -136,12 +200,19 @@ class InstructBase(nn.Module):
             [el['span_label'] for el in batch], batch_first=True, padding_value=-1
         )
 
+        if os.environ['TASK'] == 'rel':
+            rel_label = pad_sequence(
+                [el['rel_label'] for el in batch], batch_first=True, padding_value=-1
+            )
+        
+
         return {
             'seq_length': torch.LongTensor([el['seq_length'] for el in batch]),
             'span_idx': span_idx,
             'tokens': [el['tokens'] for el in batch],
             'span_mask': span_label != -1,
             'span_label': span_label,
+            'rel_label': rel_label if 'rel_label' in locals() else None,
             'entities': [el['entities'] for el in batch],
             'classes_to_id': class_to_ids,
             'id_to_classes': id_to_classes,
@@ -151,12 +222,23 @@ class InstructBase(nn.Module):
     def get_negatives(batch_list, sampled_neg=5):
         ent_types = []
         for b in batch_list:
-            types = set([el[-1] for el in b['ner']])
+            types = set([el[2] for el in b['ner']])
             ent_types.extend(list(types))
         ent_types = list(set(ent_types))
         # sample negatives
         random.shuffle(ent_types)
         return ent_types[:sampled_neg]
+    
+    @staticmethod
+    def get_negatives_rel(batch_list, sampled_neg=5):
+        rel_types = []
+        for b in batch_list:
+            types = set([el['relation_text'] for el in b['relations']])
+            rel_types.extend(list(types))
+        rel_types = list(set(rel_types))
+        # sample negatives
+        random.shuffle(rel_types)
+        return rel_types[:sampled_neg]
 
     def create_dataloader(self, data, entity_types=None, **kwargs):
         return DataLoader(data, collate_fn=lambda x: self.collate_fn(x, entity_types), **kwargs)
