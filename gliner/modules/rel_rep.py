9d21cf9e8ba599d28e6a5f89e6b5d0be08070f51
Jack Boylan <boylan.jack@gmail.com>
2024-04-04 19:49:11 +0200

    Puts model pair generation into separate method to enable testing

--- gliner/modules/rel_rep.py
+++ gliner/modules/rel_rep.py
@@ -32,24 +32,10 @@ class SpanMarkerV1(nn.Module):
         return self.out_project(cat) # ([B, number_of_entities, D])               #### .view(B, L, self.max_width, D)
     
 
-class RelMarkerv0(nn.Module):
-    """
-    Efficiently marks and projects representations for all pairs of entities.
-    """
-    def __init__(self, hidden_size: int, max_width: int, dropout: float = 0.4):
-        super().__init__()
-        self.span_marker = SpanMarkerV1(hidden_size, max_width, dropout)
-
-        self.out_project = create_projection_layer(hidden_size * 2, dropout, hidden_size)
-
-    def forward(self, h: torch.Tensor, span_idx: torch.Tensor) -> torch.Tensor:
-        """
-        h: torch.Tensor - The hidden states of the shape [batch_size, seq_len, hidden_size]
-        span_idx: torch.Tensor - The span indices of entities of the shape [batch_size, num_entities, 2]
-        """
-        B, L, D = h.size()
-        B, num_entities, _ = span_idx.size()
-        entity_reps = self.span_marker(h, span_idx)  #  ([B, number_of_entities, D])  
+def get_entity_pair_reps(entity_reps):
+        # entity_reps.shape  -->  ([B, number_of_entities, D])  
+        
+        num_entities = entity_reps.size(1)
 
         # Create a tensor [B, num_entities, num_entities, D] by repeating entity_reps for pairing
         # Expanding entity_reps to pair each with every other
@@ -66,6 +52,30 @@ class RelMarkerv0(nn.Module):
         triu_mask = torch.triu(torch.ones(num_entities, num_entities), diagonal=1).bool()
         combined_pairs = pair_reps[:, triu_mask]
 
+        return combined_pairs
+
+
+class RelMarkerv0(nn.Module):
+    """
+    Marks and projects representations for all pairs of entities.
+    """
+    def __init__(self, hidden_size: int, max_width: int, dropout: float = 0.4):
+        super().__init__()
+        self.span_marker = SpanMarkerV1(hidden_size, max_width, dropout)
+
+        self.out_project = create_projection_layer(hidden_size * 2, dropout, hidden_size)
+
+    def forward(self, h: torch.Tensor, span_idx: torch.Tensor) -> torch.Tensor:
+        """
+        h: torch.Tensor - The hidden states of the shape [batch_size, seq_len, hidden_size]
+        span_idx: torch.Tensor - The span indices of entities of the shape [batch_size, num_entities, 2]
+        """
+        B, L, D = h.size()
+        # [B, num_entities, 2]  -->  span_idx.size()
+        entity_reps = self.span_marker(h, span_idx)  #  ([B, number_of_entities, D])  
+
+        combined_pairs = get_entity_pair_reps(entity_reps)
+
         # combined_pairs is now a tensor of shape [batch_size, num_pairs, 2*hidden_size]
         # where num_pairs is num_entities * (num_entities - 1) / 2, the number of unique pairs.
 
@@ -73,10 +83,3 @@ class RelMarkerv0(nn.Module):
 
 
         return combined_pairs_out
-
-# Example usage:
-# h is the hidden states from a transformer model, shape: [batch_size, seq_len, hidden_size]
-# span_idx is a tensor containing the start and end indices for each entity, shape: [batch_size, num_entities, 2]
-# efficient_entity_pairs_marker = EfficientEntityPairsMarker(hidden_size, max_width)
-# efficient_entity_pairs_rep = efficient_entity_pairs_marker(h, span_idx)
-# efficient_entity_pairs_rep now contains representations for all possible entity pairs
