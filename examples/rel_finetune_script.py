9301563439ebc7eb6e45ecad7176e9883635e015
Jack Boylan <boylan.jack@gmail.com>
2024-04-07 16:30:35 +0100

    Updates rel finetune script

--- examples/rel_finetune_script.py
+++ examples/rel_finetune_script.py
@@ -10,8 +10,6 @@ import os
 from types import SimpleNamespace
 from gliner.modules.rel_rep import RelMarkerv0
 
-os.environ['TASK'] = 'rel'
-
 
 # read jsonl file from data
 data = []
@@ -55,42 +53,44 @@ model.set_sampling_params(
     max_len=config.max_len
 )
 
+os.environ['TASK'] = 'rel'
+
 ## Predict
 
-# text = """
-# Cristiano Ronaldo dos Santos Aveiro (Portuguese pronunciation: [kɾiʃˈtjɐnu ʁɔˈnaldu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team. Widely regarded as one of the greatest players of all time, Ronaldo has won five Ballon d'Or awards,[note 3] a record three UEFA Men's Player of the Year Awards, and four European Golden Shoes, the most by a European player. He has won 33 trophies in his career, including seven league titles, five UEFA Champions Leagues, the UEFA European Championship and the UEFA Nations League. Ronaldo holds the records for most appearances (183), goals (140) and assists (42) in the Champions League, goals in the European Championship (14), international goals (128) and international appearances (205). He is one of the few players to have made over 1,200 professional career appearances, the most by an outfield player, and has scored over 850 official senior career goals for club and country, making him the top goalscorer of all time.
-# """
+text = """
+Cristiano Ronaldo dos Santos Aveiro (Portuguese pronunciation: [kɾiʃˈtjɐnu ʁɔˈnaldu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team. Widely regarded as one of the greatest players of all time, Ronaldo has won five Ballon d'Or awards,[note 3] a record three UEFA Men's Player of the Year Awards, and four European Golden Shoes, the most by a European player. He has won 33 trophies in his career, including seven league titles, five UEFA Champions Leagues, the UEFA European Championship and the UEFA Nations League. Ronaldo holds the records for most appearances (183), goals (140) and assists (42) in the Champions League, goals in the European Championship (14), international goals (128) and international appearances (205). He is one of the few players to have made over 1,200 professional career appearances, the most by an outfield player, and has scored over 850 official senior career goals for club and country, making him the top goalscorer of all time.
+"""
 
-# nlp = spacy.load('en_core_web_sm')
-# doc = nlp(text)
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(text)
 
-# ner = []
-# for ent in doc.ents:
-#     ner.append([ent.start_char, ent.end_char, ent.label_, ent.text])
-
-
-# labels = [
-#     "is parent of",
-#     "is capital of",
-#     "founded by",
-#     "works for",
-#     "located in",
-#     "causes",
-#     "is a type of",
-#     "inhibits",
-#     "discovered by",
-#     "owns",
-#     "invests in"
-# ]
-
-# relations = model.predict_entities(text, labels, threshold=0.5, ner=ner)
+ner = []
+for ent in doc.ents:
+    ner.append([ent.start, ent.end, ent.label_, ent.text])
 
 
+labels = [
+    "is parent of",
+    "is capital of",
+    "founded by",
+    "works for",
+    "located in",
+    "causes",
+    "is a type of",
+    "inhibits",
+    "discovered by",
+    "owns",
+    "invests in"
+]
 
-# for rel in relations:
-#     print(f"{rel['head_text']} --> {rel['label']} --> {rel['tail_text']}")
+tokens = [tok.text for tok in doc]
 
-# import ipdb;ipdb.set_trace()
+relations = model.predict_entities(tokens, labels, threshold=0.5, ner=ner)
+
+for rel in relations:
+    print(f"{rel['head_text']} --> {rel['label']} --> {rel['tail_text']}")
+
+import ipdb;ipdb.set_trace()
 
 ## Train
 
