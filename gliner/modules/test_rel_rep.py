871446ac7db73c0720fb6eae6a7753d1ff2c58c8
Jack Boylan <boylan.jack@gmail.com>
2024-04-09 08:28:36 +0000

    Modifies test to reflect bidirectional entity pairs

--- gliner/modules/test_rel_rep.py
+++ gliner/modules/test_rel_rep.py
@@ -27,14 +27,100 @@ def test_forward_output_matches_data_processing():
             [[900, 900], [901, 901], [902, 902], [903, 903], [904, 904]],
         ]
     )      
-    #  -->  ([B, number_of_entities, 2])  
+    #  -->  ([B, number_of_entities, 2])  ([4, 5, 2])
+
+    # we expect there to be head-tail entity pairs in both directions, but no self-pairs
+    expected_output = torch.tensor([[[  1,   1,   2,   2],
+         [  1,   1,   3,   3],
+         [  1,   1,   4,   4],
+         [  1,   1,   5,   5],
+         [  2,   2,   1,   1],
+         [  2,   2,   3,   3],
+         [  2,   2,   4,   4],
+         [  2,   2,   5,   5],
+         [  3,   3,   1,   1],
+         [  3,   3,   2,   2],
+         [  3,   3,   4,   4],
+         [  3,   3,   5,   5],
+         [  4,   4,   1,   1],
+         [  4,   4,   2,   2],
+         [  4,   4,   3,   3],
+         [  4,   4,   5,   5],
+         [  5,   5,   1,   1],
+         [  5,   5,   2,   2],
+         [  5,   5,   3,   3],
+         [  5,   5,   4,   4]],
+
+        [[ 21,  21,  22,  22],
+         [ 21,  21,  23,  23],
+         [ 21,  21,  24,  24],
+         [ 21,  21,  25,  25],
+         [ 22,  22,  21,  21],
+         [ 22,  22,  23,  23],
+         [ 22,  22,  24,  24],
+         [ 22,  22,  25,  25],
+         [ 23,  23,  21,  21],
+         [ 23,  23,  22,  22],
+         [ 23,  23,  24,  24],
+         [ 23,  23,  25,  25],
+         [ 24,  24,  21,  21],
+         [ 24,  24,  22,  22],
+         [ 24,  24,  23,  23],
+         [ 24,  24,  25,  25],
+         [ 25,  25,  21,  21],
+         [ 25,  25,  22,  22],
+         [ 25,  25,  23,  23],
+         [ 25,  25,  24,  24]],
+
+        [[800, 800, 801, 801],
+         [800, 800,   0,   0],
+         [800, 800,   0,   0],
+         [800, 800,   0,   0],
+         [801, 801, 800, 800],
+         [801, 801,   0,   0],
+         [801, 801,   0,   0],
+         [801, 801,   0,   0],
+         [  0,   0, 800, 800],
+         [  0,   0, 801, 801],
+         [  0,   0,   0,   0],
+         [  0,   0,   0,   0],
+         [  0,   0, 800, 800],
+         [  0,   0, 801, 801],
+         [  0,   0,   0,   0],
+         [  0,   0,   0,   0],
+         [  0,   0, 800, 800],
+         [  0,   0, 801, 801],
+         [  0,   0,   0,   0],
+         [  0,   0,   0,   0]],
+
+        [[900, 900, 901, 901],
+         [900, 900, 902, 902],
+         [900, 900, 903, 903],
+         [900, 900, 904, 904],
+         [901, 901, 900, 900],
+         [901, 901, 902, 902],
+         [901, 901, 903, 903],
+         [901, 901, 904, 904],
+         [902, 902, 900, 900],
+         [902, 902, 901, 901],
+         [902, 902, 903, 903],
+         [902, 902, 904, 904],
+         [903, 903, 900, 900],
+         [903, 903, 901, 901],
+         [903, 903, 902, 902],
+         [903, 903, 904, 904],
+         [904, 904, 900, 900],
+         [904, 904, 901, 901],
+         [904, 904, 902, 902],
+         [904, 904, 903, 903]]])
 
     batch_size = span_idx.size(0)
 
     
     # Run the model's forward method for generating pairs
     rel_rep = get_entity_pair_reps(span_idx) #  -->  ([B, num_unique_pairs, 4])
-    
+
+
     # Compute pairs of entities as done during precprocessing
     base_pairs_list = []
     batch_pairs_list = []
@@ -54,4 +140,8 @@ def test_forward_output_matches_data_processing():
     
     # Assert the outputs are equal
     assert rel_rep.equal(base_pairs), "get_entity_pair_reps and generate_entity_pairs_indices do not give the same output"
+    assert rel_rep.equal(expected_output), "get_entity_pair_reps does not match expected_output"
 
+
+if __name__ == "__main__":
+    test_forward_output_matches_data_processing()
